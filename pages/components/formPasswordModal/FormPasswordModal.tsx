e51630f258135f64c1c4ba010da1adca27c1b1d5
Paula Cano <paulacanoroman@gmail.com>
2024-07-10 09:46:05 +0200

    feat: useState save data user password

--- pages/components/formPasswordModal/FormPasswordModal.tsx
+++ pages/components/formPasswordModal/FormPasswordModal.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormPasswordModal = () => {
+
+    const [userPassword, setUserPassword] = useState<string>('')
+    console.log(userPassword)
   return (
     <>
-    <input placeholder='Write your password' type='password'></input>
+    <input placeholder='Write your password' type='password' onChange={(e)=>{setUserPassword(e.target.value)}}></input>
     </>
   )
 }
